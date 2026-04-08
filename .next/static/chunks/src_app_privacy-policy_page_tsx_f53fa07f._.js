ec93056215ca90f70e0467583e0d3165fa1d4874
Salma M <salma@Noordeens-MacBook-Air.local>
2026-04-08 11:49:21 +0530

    Fix sitemap timeout and update gitignore

--- .next/static/chunks/src_app_privacy-policy_page_tsx_f53fa07f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_a825d28e._.js",
-  "static/chunks/node_modules_fae12fb3._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_testimonials_page_tsx_f53fa07f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_db031dd0._.js",
-  "static/chunks/node_modules_aa6a064f._.js"
-],
-    source: "dynamic"
-});
